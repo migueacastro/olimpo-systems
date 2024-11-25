47314b551d4616e2cf243ee6d342a9ffa68007fe
Miguel Castro <54249734+migueacastro@users.noreply.github.com>
2024-11-25 19:46:07 +0000

    Avances en viewsets

--- frontend/src/routes/tecnicos/+page.svelte
+++ frontend/src/routes/tecnicos/+page.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
 	import Datatable from '$lib/components/Datatable.svelte';
     import { initializeStores } from '@skeletonlabs/skeleton';
+	import { apiEndpoint } from '$lib/endpoint';
     initializeStores();
 	
+	let id: any = '';
+	let cedula: any = '';
+	let nombres: any = '';
+	let apellidos: any = '';
+	let telefono: any = '';
+
+	$: showForm = false;
+
+	let handleSubmit = () => {
+        const endpoint = apiEndpoint + 'dispositivos/' ;
+        let data = new FormData();
+        data.append('marca', cedula);
+        data.append('modelo', nombres);
+        data.append('serial', apellidos);
+        data.append('tipo', telefono);
+
+        fetch(endpoint, {
+            method: 'POST',
+            body: data,
+        })
+        .then(response => {
+            if (response.ok) {
+                showForm = false;
+                window.location.reload();
+            } else {
+                let data = response.json();
+                getModalStore().trigger({
+                    type: 'alert',
+                    title: 'Error'
+                })
+            }
+        })
 </script>
 
 <div class="space-y-10">
@@ -13,7 +46,6 @@
 			<button
 					type="button"
 					class="btn btn-md variant-filled-primary border border-gray-500"
-	
 				>
 					<span>Agregar</span>
 				</button>
